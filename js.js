8b3bf786bfaa8b015bf2278f67609204ac2ec1cc
Kayk-Rios <manjac688@gmail.com>
2024-05-02 02:35:54 -0300

    Clone_Iphone

---
+++ js.js
@@ -0,0 +1,10 @@
+function imgSlid(e) {
+    document.querySelector('#phone').src = e;
+
+}
+
+function circle(color) {
+    const circle = document.querySelector('.circle')
+    circle.style.background = color
+    
+}
